c35160a7b2213b4a7fc3dcd52b7e7c388e30f43a
Mayank Mohit <mayank.mohit1122@gmail.com>
2022-11-08 23:52:04 +0530

    Initial commit to git repo

---
+++ src/main/java/development/mayank/chatapplication/controller/ChatController.java
@@ -0,0 +1,33 @@
+package development.mayank.chatapplication.controller;
+
+import development.mayank.chatapplication.model.ChatMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@Slf4j
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        chatMessage.setType(ChatMessage.MessageType.CHAT);
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Addition of username in web socket session
+        log.info("");
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
